cad1adecc749b8556e9fc9abf37b2fb14f69477b
MatthewS3 <mtthwswain@gmail.com>
2024-02-28 16:22:39 +0200

    login and contact page

--- techtopiafrontend/src/views/AboutView.vue
+++ techtopiafrontend/src/views/AboutView.vue
@@ -3,102 +3,178 @@
     <div class="left">
       <h1>About us</h1>
       <hr>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>
+        At TechTopia, we're more than just an e-commerce platform; we're your gateway to a world of innovation and
+        convenience.
+        Founded by Mr Joel Williams & Mr Matthew Swain with and amazing passion and an prospering goal to be successful
+        entreprenuers with a simple yet powerful vision: to make technology accessible to everyone, everywhere. Whether
+        you're a gadget guru, a tech novice, or somewhere in between, we're here to cater to your needs and elevate your
+        tech experience.
+      </p>
+      <p>
+        At TechTopia, we're committed to fostering a community of tech lovers and innovators. We believe in the
+        transformative power of technology to improve lives and drive progress,
+        We understand that technology can sometimes feel intimidating or overwhelming, which is why we're committed to
+        creating a welcoming and inclusive space where everyone.
+        Whether you're here to upgrade your gear, discover the latest trends, or simply geek out over the newest gadgets,
+        TechTopia is your go-to destination for all things tech. Join us on this exciting journey as we explore the
+        endless possibilities of the digital age together.
+      </p>
 
     </div>
     <div class="right">
-      <img src="https://res.cloudinary.com/arpr/image/upload/v1540359700/adult-affection-baby-755028.jpg">
+      <img src="https://cdn-images.imagevenue.com/60/19/3b/ME17PLBX_o.jpg">
     </div>
     <div class="clear"></div>
   </div>
   <div class="mission">
 
     <div class="left">
-      <img src="https://res.cloudinary.com/arpr/image/upload/v1540359700/adult-affection-baby-755028.jpg">
+      <img src="https://cdn-images.imagevenue.com/5e/d1/50/ME17PLBN_o.jpg">
     </div>
     <div class="right">
       <h1>Our Mission!</h1>
       <hr>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>
+        Our mission is to empower tech enthusiasts, professionals, and everyday users alike with the latest and most
+        cutting-edge technology products.
+        to democratize technology and foster a world where everyone can harness its transformative power. We believe that
+        access to technology is not just a privilege but a fundamental right, and we're committed to breaking down
+        barriers and bridging the digital divide.
+        Through our curated selection of products, educational resources, and community initiatives, we aim to empower
+        individuals of all backgrounds to embrace technology with confidence and enthusiasm. Whether you're a student
+        striving for academic success, an entrepreneur building your dream business, or a senior exploring the wonders of
+        the digital realm, we're here to support you every step of the way.
+      </p>
+      <p>
+        Together, let's build a brighter tomorrow—one byte at a time. Welcome to TechTopia, where technology meets
+        possibility, and everyone is invited to join the journey.
+      </p>
     </div>
-    <div class="clear"></div>
   </div>
   <div class="team">
     <h1> Our Team
       <hr>
     </h1>
-    <div class="card">
-      <div class="circle-container">
-        <h1 style="color:#fff;">M</h1>
-      </div>
-      <h2>Mahesh E U</h2>
-      <h4>Technical Lead</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      </p>
-    </div>
-    <div class="card">
-      <div class="circle-container">
-        <h1 style="color:#fff;">M</h1>
-      </div>
-      <h2>Mridul</h2>
-      <h4>Evangelist</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      </p>
-    </div>
+    <div class="container6">
+      <div class="grid">
+        <div class="card">
+          <input id="card2" type="checkbox" />
+          <label class="tgl-btn" for="card2"><span></span></label>
+          <div class="tgl-view">
+            <div class="card-image">
+              <img src="" alt="tamlin" />
+            </div>
+            <h2 class="card-title">Joel Williams</h2>
+            <p class="card-detail">
+              "Working with Matthew has been an absolute pleasure, and I couldn't be more
+              grateful for their invaluable contributions to our web development projects.
+              He is not just a talented developer, but a great friend. Their passion for web
+              development is evident in every task they undertake.He approaches each project
+              with enthusiasm and dedication, consistency. What truly stands out is
+              Matthew's helpful nature, hi always ready to lend a hand to his friends.
+            </p>
+          </div>
+        </div>
 
+        <div class="card">
+          <input id="card3" type="checkbox" />
+          <label class="tgl-btn" for="card3"><span></span></label>
+          <div class="tgl-view">
+            <div class="card-image">
+              <img src="https://cdn-images.imagevenue.com/14/07/b8/ME17PLS6_o.png" alt="aakeefah" />
+            </div>
+            <h2 class="card-title">Matthew Swain</h2>
+            <p class="card-detail">
+              "Founder and Co-Owner of TechTopia Matthew Swain."
+            </p>
+          </div>
+        </div>
+        <div class="card">
+          <input id="card1" type="checkbox" />
+          <label class="tgl-btn" for="card1"><span></span></label>
+          <div class="tgl-view">
+            <div class="card-image">
+              <img src="https://i.postimg.cc/nh7CTq4z/20240110-140911.jpg" alt="veronique" />
+            </div>
+            <h2 class="card-title">Veronique Nappie</h2>
+            <p class="card-detail">
+              Matthew has been a continuous surprise for me. I've gotten to know him a lot
+              better over the past few weeks and I've seen a different side to him. He seems
+              to handle pressure very well and puts a lot of effort into 'getting the job
+              done'. He is incredibly professional and has so much creativity to share. I
+              hope he continues on this path because he has a lot to give and I'd like for
+              everyone to see that.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container6">
+      <div class="grid">
+        <div class="card">
+          <input id="card4" type="checkbox" />
+          <label class="tgl-btn" for="card4"><span></span></label>
+          <div class="tgl-view">
+            <div class="card-image">
+              <img src="https://i.postimg.cc/TPnxZ91y/Clipped-image-20240111-083818-1.png" alt="JNR" />
+            </div>
+            <h2 class="card-title">Unity Demas</h2>
+            <p class="card-detail">
+              Matt's like the energizer bunny of hard work and focus - he tackles everything
+              with this contagious enthusiasm that's impossible to ignore. Whether he's
+              grinding away at work or just being an all-around awesome friend, Matt's the
+              guy you want in your corner. His dedication is unmatched, and he's got this
+              knack for turning even mundane tasks into something fun. I'm grateful to have
+              him as a friend; he's the kind of person who makes life way more interesting
+              and a whole lot easier.
+            </p>
+          </div>
+        </div>
 
+        <div class="card">
+          <input id="card5" type="checkbox" />
+          <label class="tgl-btn" for="card5"><span></span></label>
+          <div class="tgl-view">
+            <div class="card-image">
+              <img src="https://i.postimg.cc/DwtwX0RT/1000025632.jpg" alt="mr raffels" />
+            </div>
+            <h2 class="card-title">Damon Raffels</h2>
+            <p class="card-detail">
+              "Matthew is truly a distinct individual. Reserved, calm while simultaneously
+              being one of the most joyous & fun people you will have the pleasure of
+              meeting and working with. He has unrivaled creativity and curiosity, always
+              furthering his knowledge skills & driving others. He has excellent
+              communication skills and will be an asset to any workforce. His passion and
+              dedication will take him as far as he wants to go. I am truly proud and
+              honored to call him my colleague and most importantly, my close friend
+            </p>
+          </div>
+        </div>
 
-    <div class="card">
-      <div class="circle-container">
-        <h1 style="color:#fff;">R</h1>
+        <div class="card">
+          <input id="card6" type="checkbox" />
+          <label class="tgl-btn" for="card6"><span></span></label>
+          <div class="tgl-view">
+            <div class="card-image">
+              <img src="https://i.postimg.cc/qqTBjGL8/profile-portfolio.jpg" alt="rezaar" />
+            </div>
+            <h2 class="card-title">Rezaar Osman</h2>
+            <p class="card-detail">
+              Matthew's passion for the IT industry shines through in every aspect of his
+              work. His diligence and professional attitude contribute to the creation of
+              flawless end results. Matthew's commitment to maintaining high standards sets
+              him apart, ensuring that his work consistently meets or exceeds expectations.
+              What sets him apart is his drive to be unique in his work environment, always
+              seeking ways to innovate and improve. Beyond his professional attributes,
+              Matthew is a great person to befriend, radiating positive energy and making
+              the work atmosphere enjoyable. In summary, Matthew's combination of passion,
+              diligence, high standards, and friendly demeanor make him a valuable asset to
+              any team.
+            </p>
+          </div>
+        </div>
       </div>
-      <h2>Rajesh</h2>
-      <h4>Evangelist</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      </p>
-    </div>
-    <div class="card">
-      <div class="circle-container">
-        <h1 style="color:#fff;">B</h1>
-      </div>
-      <h2>Bharti</h2>
-      <h4>Evangelist</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      </p>
-    </div>
-    <div class="card">
-      <div class="circle-container">
-        <h1 style="color:#fff;">A</h1>
-      </div>
-      <h2>Anhsuman</h2>
-      <h4> Advisor</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      </p>
-    </div>
-    <div class="card">
-      <div class="circle-container">
-        <h1 style="color:#fff;">S</h1>
-      </div>
-      <h2>Shruti</h2>
-      <h4> Advisor</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      </p>
     </div>
   </div>
 </template>
@@ -127,7 +203,8 @@
 }
 
 .about .right img {
-  width: 100%
+  width: 100%;
+  height: 500px;
 }
 
 .about .left h1 {
@@ -171,6 +248,8 @@
 
 .mission .left img {
   width: 100%;
+  height: 500px;
+
 }
 
 .mission .right h1 {
@@ -195,7 +274,7 @@
   width: 100%;
   margin: auto;
   padding: 5% 0;
-  background-color: #ddd;
+  background-color: #fff;
 }
 
 .team h1 {
@@ -207,63 +286,225 @@
 .team h1 hr {
   width: 100%;
   border-top: 2px solid #000;
-  background-color: transperant;
+  background-color: transparent;
   border-bottom: none;
   margin-bottom: 50px;
 }
 
-.team .card {
-  padding-bottom: 20px;
-  padding-top: 20px;
-  position: relative;
-  min-width: 20%;
-  height: 500px !important;
-  height: auto;
-  background-color: #fff;
-  display: inline-block;
-  margin: 40px 6.5%;
-  box-shadow: 0 0 20px 2px rgba(0, 0, 0, 0.5);
-  text-align: center;
+.grid {
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
 }
 
-.card .circle-container {
+.card {
+  height: 450px;
+  margin: 0 0 20px;
   position: relative;
-  background-color: #579f79;
-  width: 250px;
-  height: 250px;
-  border: none;
-  border-radius: 50%;
-  margin: auto;
-  color: #fff;
 }
 
-.circle-container h1 {
+.card .card-image {
+  background: #222;
+  display: inline-flex;
+  overflow: hidden;
+  height: 400px;
+  width: auto;
+  transition: all 1s cubic-bezier(1, -0.28, 0.66, 0.48);
+  position: relative;
+  z-index: 1;
+}
+
+.card .card-image img {
+  height: 433px;
+  width: 372px;
+  object-fit: cover;
+  transition: all 1s cubic-bezier(1, -0.28, 0.66, 0.48);
+}
+
+.card .card-title {
+  background: tomato;
+  color: black;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0);
+  transition: all 0.66s cubic-bezier(1, -0.28, 0.66, 0.48);
+  margin: -5px 0 0 -20px;
+  padding: 10px 20px 10px 20px;
+  position: relative;
+}
+
+.card .card-title::before {
   position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  top: -20px;
+  left: 0;
+  content: "";
+  display: block;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 0 0 20px 20px;
+  border-color: transparent transparent tomato transparent;
+  transition: all 0.66s cubic-bezier(1, -0.28, 0.66, 0.48);
+  z-index: -1;
+}
+
+.card .card-title::after {
+  position: absolute;
+  bottom: 0px;
+  left: 0;
+  content: "";
+  display: block;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 20px 0 20px 20px;
+  border-color: tomato transparent transparent transparent;
+  transition: all 1s cubic-bezier(1, -0.28, 0.66, 0.48);
+  z-index: -1;
+}
+.card .card-detail {
+  background: #111;
+  height: auto;
   margin: 0;
-  font-size: 60px;
+  color: #ddd;
+  transition: all 1s cubic-bezier(1, -0.28, 0.66, 0.48);
 }
 
-.team .card h2 {
-  text-align: center;
-  margin: 30px 0 0 0;
+.card .tgl-btn {
+  background: tomato;
+  position: absolute;
+  cursor: pointer;
+  display: block;
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+  z-index: 99;
+  top: 10px;
+  right: 10px;
+  transition: all 1s cubic-bezier(1, -0.28, 0.66, 0.48);
 }
 
-.team .card h4 {
-  text-align: center;
-  margin: 10px 0 0 0;
+.card .tgl-btn span {
+  background: #000;
+  display: block;
+  height: 0px;
+  width: 30px;
+  margin: 10px;
+  position: relative;
+  transition: all 0.66s cubic-bezier(1, -0.28, 0.66, 0.48);
 }
 
-.team .card p {
-  width: 250px;
-  margin: 30px auto;
-  text-align: center;
-
+.card .tgl-btn span::before,
+.card .tgl-btn span::after {
+  content: "";
+  background: #000;
+  display: block;
+  height: 1.9px;
+  width: 30px;
+  position: absolute;
+  transition: all 0.66s cubic-bezier(1, -0.28, 0.66, 0.48);
 }
 
-.clear {
-  clear: both;
+.card .tgl-btn span::before {
+  bottom: -20px;
+}
+
+.card .tgl-btn span::after {
+  bottom: -11px;
+}
+
+.card [type="checkbox"] {
+  display: none;
+}
+
+.card [type="checkbox"]:checked+.tgl-btn {
+  background: transparent;
+}
+
+.card [type="checkbox"]:checked+.tgl-btn span {
+  background: black;
+  width: 0;
+}
+
+.card [type="checkbox"]:checked+.tgl-btn span::before {
+  bottom: -5px;
+  width: 25px;
+  transform: rotate(45deg);
+}
+
+.card [type="checkbox"]:checked+.tgl-btn span::after {
+  bottom: -5px;
+  width: 25px;
+  transform: rotate(-45deg);
+}
+
+.card [type="checkbox"]:not(:checked)~.tgl-view .card-image {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+}
+
+.card [type="checkbox"]:not(:checked)~.tgl-view .card-detail {
+  opacity: 0;
+  overflow: hidden;
+  padding: 0;
+  min-height: 0;
+  max-height: 0;
+}
+
+.card [type="checkbox"]:checked~.tgl-view .card-image {
+  border-radius: 30px;
+  height: 60px;
+  width: 60px;
+  margin: 10px 0;
+}
+
+.card [type="checkbox"]:checked~.tgl-view .card-image img {
+  height: 60px;
+}
+
+.card [type="checkbox"]:checked~.tgl-view .card-title {
+  font-size: 18px;
+  line-height: 60px;
+  margin-top: -80px;
+  margin-left: -10px;
+  padding: 10px 10px 10px 80px;
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.1);
+}
+
+.card [type="checkbox"]:checked~.tgl-view .card-title::before {
+  top: 0px;
+}
+
+.card [type="checkbox"]:checked~.tgl-view .card-title::after {
+  bottom: -40px;
+}
+
+.card [type="checkbox"]:checked~.tgl-view .card-detail {
+  opacity: 1;
+  padding: 40px 20px;
+  min-height: 290px;
+  max-height: 400px;
+}
+
+@media only screen and (min-width: 500px) {
+  .grid {
+    margin-left: -20px;
+    margin-right: -20px;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  .card {
+    margin: 20px;
+  }
+}
+
+@media only screen and (min-width: 900px) {
+  .container6 {
+    margin: 80px auto;
+    max-width: 1200px;
+    width: calc(100% - 160px);
+  }
+
+  .grid {
+    grid-template-columns: repeat(3, 1fr);
+  }
 }
 </style>
